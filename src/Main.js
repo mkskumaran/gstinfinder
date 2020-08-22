dd5c912d69b1488a094da6cc53da711db9935864
ShunmugaKumaran M <34643845+mkskumaran@users.noreply.github.com>
2020-08-22 07:37:40 +0530

    Updated

--- src/Main.js
+++ src/Main.js
@@ -3,11 +3,6 @@ import { Switch, Route } from 'react-router-dom'
 import App from './App'
 import Gst from './Gst'
 import Drawer from './Drawer'
-// The Main component renders one of the three provided
-// Routes (provided that one matches). Both the /roster
-// and /schedule routes will match any pathname that starts
-// with /roster or /schedule. The / route will only match
-// when the pathname is exactly the string "/"
 
 const Main = () => (  
 <Drawer>

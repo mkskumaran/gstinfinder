826e63f5261aca0cbe458040d7f50551994fda83
ShunmugaKumaran M <34643845+mkskumaran@users.noreply.github.com>
2020-08-30 01:13:05 +0530

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import { Switch, Route , BrowserRouter as Router} from 'react-router-dom'
+import { Switch, Route , BrowserRouter} from 'react-router-dom'
 import Gst from './Gst'
 import Gst_new from './Gst_new'
 import Drawer from './Drawer'
 
 const Main = () => (  
 <Drawer>
-<Router basename="/">
+<BrowserRouter basename="/">
     <Switch>
       <Route exact path='/' component={Gst}/>
 	  <Route path='/gstinfinder/gst_new' component={Gst_new}/>
     </Switch>
-</Router>	
+</BrowserRouter>	
  </Drawer> 
 )
 

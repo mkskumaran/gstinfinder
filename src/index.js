2ee3926633be0280b801df5a0e25b35074ab466a
ShunmugaKumaran M <34643845+mkskumaran@users.noreply.github.com>
2020-08-30 01:20:07 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Main from './Main';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter } from 'react-router-dom'
 //import './i18n';
 
-ReactDOM.render(
-<BrowserRouter>
-<Main />
-</BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<Main />, document.getElementById('root'));
 registerServiceWorker();
